6a3712c060d2f93e85ad829eec6be0b3b816443b
Eze Vazquez <eze@Ezes-MacBook-Air.local>
2023-10-10 20:14:47 -0300

    sigo probando por qué Vercel no arranca

--- apps/client/src/index.tsx
+++ apps/client/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './components/navbar'
-import App from './components/app'
 import './index.css'
+import App from './components/app'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
