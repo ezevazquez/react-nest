47f6888a70e51d932fec263f4fcd6348db259404
Eze Vazquez <eze@Ezes-MacBook-Air.local>
2023-10-10 20:18:52 -0300

    elimino app.tsx y app.css

--- apps/client/src/index.tsx
+++ apps/client/src/index.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './components/navbar'
 import './index.css'
-import App from './components/app'
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Navbar />
-    <App />
   </React.StrictMode>,
 );
