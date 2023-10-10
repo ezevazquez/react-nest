e1b4cb0c57c244a24b7dcf57d2fc87b3b2ce4432
Eze Vazquez <eze@Ezes-MacBook-Air.local>
2023-10-10 20:34:14 -0300

    add body text

---
+++ apps/client/src/components/body.tsx
@@ -0,0 +1,9 @@
+function Body() {
+  return (
+    <div className="body">
+      <h1>Aguantame Jesi que estoy haciendo unos cambiecitos</h1>
+    </div>
+  );
+}
+
+export default Body;
--- apps/client/src/index.tsx
+++ apps/client/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './components/navbar'
+import Body from './components/body'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Navbar />
+    <Body />
   </React.StrictMode>,
 );
