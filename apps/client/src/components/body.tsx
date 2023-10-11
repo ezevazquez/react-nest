71aed72177ce0e6622530a7199c02b9bdd28c85f
Eze Vazquez <eze@Ezes-MacBook-Air.local>
2023-10-10 22:00:18 -0300

    add social card, second try

--- apps/client/src/components/body.tsx
+++ apps/client/src/components/body.tsx
@@ -1,19 +1,15 @@
 import "./body.css";
 import { Helmet } from 'react-helmet';
-import siteConfig from "../siteConfig";
 
 function Body() {
   return (
     <div className="body">
       <Helmet>
-        <title>{siteConfig.title}</title>
-        <meta name="description" content={siteConfig.description} />
-        <meta property="og:title" content={siteConfig.title} />
-        <meta property="og:description" content={siteConfig.description} />
-        <meta
-          property="og:image"
-          content={siteConfig.siteUrl + siteConfig.image}
-        />
+        <title>'El proyecto de Jesi'</title>
+        <meta name="description" content='El proyecto de Jesi' />
+        <meta property="og:title" content='El proyecto de Jesi' />
+        <meta property="og:description" content='Acá va la descripción de Jesi' />
+        <meta property="og:image" content='./assets/socialcard-jesi1.png' />
       </Helmet>
       <h1>Hola Jesi</h1>
     </div>
