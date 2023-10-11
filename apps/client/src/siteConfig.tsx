797dcfc0a661d94e154a45b0db1af3d1c804fc28
Eze Vazquez <eze@Ezes-MacBook-Air.local>
2023-10-10 22:06:20 -0300

    change title metadata

--- apps/client/src/siteConfig.tsx
+++
@@ -1,8 +0,0 @@
-const siteConfig = {
-    title: 'El proyecto de Jesi',
-    description: 'Acá va la descripción de Jesi',
-    siteUrl: 'https://jesi.ar',
-    image: '/assets/socialcard-jesi1.png',
-  };
-  
-  export default siteConfig;
