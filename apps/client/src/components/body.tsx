cf0eaed8565d430881cbfde7ba296344a9df569f
Eze Vazquez <eze@Ezes-MacBook-Air.local>
2023-10-10 22:13:38 -0300

    prueba nueva de social card

--- apps/client/src/components/body.tsx
+++ apps/client/src/components/body.tsx
@@ -1,14 +1,8 @@
 import "./body.css";
-import { Helmet } from 'react-helmet';
 
 function Body() {
   return (
     <div className="body">
-      <Helmet>
-        <meta property="og:title" content='El proyecto de Jesi' />
-        <meta property="og:description" content='Acá va la descripción de Jesi' />
-        <meta property="og:image" content='./assets/socialcard-jesi1.png' />
-      </Helmet>
       <h1>Hola Jesi</h1>
     </div>
   );
--- apps/client/src/components/navbar.tsx
+++ apps/client/src/components/navbar.tsx
@@ -1,8 +1,14 @@
 import './navbar.css';
+import { Helmet } from 'react-helmet';
 
 function Navbar() {
   return (
     <nav className="navbar">
+      <Helmet>
+        <meta property="og:title" content='El proyecto de Jesi' />
+        <meta property="og:description" content='Acá va la descripción de Jesi' />
+        <meta property="og:image" content='./assets/socialcard-jesi1.png' />
+      </Helmet>
       <ul className="nav-list">
         <li className="nav-item">
           <a href="/">Inicio</a>
