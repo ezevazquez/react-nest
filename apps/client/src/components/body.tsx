0c9f397e11ccda3953b4a47b3bce40c47a6d9c66
Eze Vazquez <eze@Ezes-MacBook-Air.local>
2023-10-10 21:52:02 -0300

    add social card

--- apps/client/src/components/body.tsx
+++ apps/client/src/components/body.tsx
@@ -1,8 +1,20 @@
-import './body.css'
+import "./body.css";
+import { Helmet } from 'react-helmet';
+import siteConfig from "../siteConfig";
 
 function Body() {
   return (
     <div className="body">
+      <Helmet>
+        <title>{siteConfig.title}</title>
+        <meta name="description" content={siteConfig.description} />
+        <meta property="og:title" content={siteConfig.title} />
+        <meta property="og:description" content={siteConfig.description} />
+        <meta
+          property="og:image"
+          content={siteConfig.siteUrl + siteConfig.image}
+        />
+      </Helmet>
       <h1>Hola Jesi</h1>
     </div>
   );
---
+++ apps/client/src/siteConfig.tsx
@@ -0,0 +1,8 @@
+const siteConfig = {
+    title: 'El proyecto de Jesi',
+    description: 'Acá va la descripción de Jesi',
+    siteUrl: 'https://jesi.ar',
+    image: '/assets/socialcard-jesi1.png',
+  };
+  
+  export default siteConfig;
