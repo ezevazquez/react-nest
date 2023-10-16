33e0bfca9315c4dde476c305fac40f9d53b466b2
Eze Vazquez <eze@Ezes-MacBook-Air.local>
2023-10-16 20:47:47 -0300

    add typeorm config

--- apps/client/src/index.tsx
+++ apps/client/src/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
 import './index.css'
-import ReactDOM from 'react-dom/client'
 import Navbar from './components/navbar'
 import Body from './components/body'
 import JesiImages from './components/images'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
